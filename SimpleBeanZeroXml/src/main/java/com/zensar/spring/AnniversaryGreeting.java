1c0bea74b13d786ca9741d4cc6c17f7c3e80785a
Dipankar Chakraborty <dipankarchakraborty645@gmail.com>
2019-10-09 19:45:35 +0530

    Added Project SimpleBeanZeroXml

---
+++ SimpleBeanZeroXml/src/main/java/com/zensar/spring/AnniversaryGreeting.java
@@ -0,0 +1,17 @@
+package com.zensar.spring;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnniversaryGreeting implements Greeting {
+
+	public String greet() {
+		// TODO Auto-generated method stub
+		return "Happy Anniversary";
+	}
+
+	public AnniversaryGreeting() {
+		// TODO Auto-generated constructor stub
+		System.out.println("Anniversary Greeting is created.");
+	}
+}
---
+++ SimpleBeanZeroXml/src/main/java/com/zensar/spring/main/SpringMain.java
@@ -0,0 +1,17 @@
+package com.zensar.spring.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import com.zensar.spring.Greeting;
+import com.zensar.spring.config.GreetingConfig;
+
+public class SpringMain {
+	public static void main(String[] args) {
+		//creating an object of spring ioc container
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(GreetingConfig.class);
+		Greeting g = ctx.getBean("anniversaryGreeting", Greeting.class);
+		System.out.println(g.greet());
+		Greeting g1 = ctx.getBean("festival", Greeting.class);
+		System.out.println(g1.greet());
+	}
+}
